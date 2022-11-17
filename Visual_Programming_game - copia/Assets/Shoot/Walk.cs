86554aae9654d335c7090796546e86e2c8efb0fa
rosita2423 <victorhugro@hotmail.com>
2022-11-17 07:19:12 -0600

    Octa
    7:18am 17/11/2022

---
+++ Visual_Programming_game - copia/Assets/Shoot/Walk.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+   Rigidbody rb;
+   bool floorDetected = false;
+   bool isJump = false;
+   public float jumpForce = 5.0f;
+
+   void Start()
+   {
+       rb = GetComponent<Rigidbody>();
+       Cursor.lockState = CursorLockMode.Locked;
+   }
+
+   void Update()
+   {
+       Vector3 floor = transform.TransformDirection(Vector3.down);
+
+       if (Physics.Raycast(transform.position, floor,  1.03f))
+       {
+            floorDetected = true;
+       } 
+       else
+       {
+            floorDetected = false;
+       } 
+ 
+       isJump = Input.GetButtonDown("Jump");
+       if(isJump && floorDetected)
+      {
+         rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+      }
+      
+   }
+}
